44b247bcd2a9109c190cd6c0d2908eb66daf1a0f
rs9w33 <robyn.gwee@deltares.nl>
2021-12-20 16:32:40 +0100

    ..

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.append(os.path.abspath('../..'))
 sys.path.append(os.path.abspath('..'))
-import lhat
 
 
 # -- Project information -----------------------------------------------------
