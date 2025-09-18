Fixed p_group calculation and implemented log of X

rain classification/rain_threshold.py
@@ -37,7 +37,6 @@ def calculate_cumulative_p_landslide(x_grid, y_grid, landslide_probs, p_group, t
 if __name__ == "__main__":
 
     n_grid = 50
-    prob_threshold = 0.5
     log_X = False
     # log_X = True
 
@@ -64,7 +63,8 @@ if __name__ == "__main__":
         if "occurrence" in col:
             df = df.rename(columns={col: "occurrences"})
     
-    feats = ["Intensity [mm/d]", "Duration [d]"]
+    # feats = ["Intensity [mm/d]", "Duration [d]"]
+    feats = ["Intensity [mm/d]", "Cumulative rainfall [mm]"]
     X = df[feats].values
     y = df["occurrences"].values
 
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     p_group, _, _ = np.histogram2d(X[:, 0], X[:, 1], bins=[x_edges, y_edges], density=True)
     hist_survival, _, _ = np.histogram2d(X[y == 0, 0], X[y == 0, 1], bins=[x_edges, y_edges])
     hist_landslide, _, _ = np.histogram2d(X[y == 1, 0], X[y == 1, 1], bins=[x_edges, y_edges])
-    landslide_ratio = np.where(hist_survival+hist_landslide>0 , hist_landslide/(hist_survival+hist_landslide), np.nan)
+    landslide_ratio = np.where(hist_survival+hist_landslide>0 , hist_landslide/(hist_survival+hist_landslide), 0.)
 
     p_group_test, _, _ = np.histogram2d(X_test[:, 0], X_test[:, 1], bins=[x_edges, y_edges], density=True)
     hist_survival_test, _, _ = np.histogram2d(X_test[y_test == 0, 0], X_test[y_test == 0, 1], bins=[x_edges, y_edges])
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     volume_p_landslide_test = trapezoid(volume_p_landslide_test, x_grid, axis=-1)
 
     f = lambda x: calculate_p_landslide(x_grid, y_grid, landslide_probs, p_group, x)
-    threshold_params = np.linspace(0, 5_000, 100_000)
+    threshold_params = np.linspace(0, 5_000, 10_000)
     p_landslide = list(map(f, threshold_params))
     p_diff = [np.abs(volume_p_landslide_test-p).item() for p in p_landslide]
     opt_threshold_params = threshold_params[p_diff.index(min(p_diff))]
@@ -115,18 +115,18 @@ if __name__ == "__main__":
         y_edges = np.exp(y_edges) - 1
 
     fig = plt.figure()
-    plt.imshow(hist_survival.T+hist_landslide.T, origin="lower", extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]], aspect="auto", cmap="Reds")
+    plt.imshow(hist_survival+hist_landslide, origin="lower", extent=[x_edges[1], x_edges[-1], y_edges[1], y_edges[-1]], aspect="auto", cmap="Reds")
     plt.colorbar(label=r"#Occurences")
     plt.xlabel(feats[0], fontsize=12)
     plt.ylabel(feats[1], fontsize=12)
     if log_X:
         plt.xscale("log")
         plt.yscale("log")
-    plt.savefig(result_path/"number_occurences.png")
+    plt.savefig(result_path/"number_occurrences.png")
     plt.close()
 
     fig = plt.figure()
-    plt.imshow(hist_landslide.T, origin="lower", extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]], aspect="auto", cmap="Reds")
+    plt.imshow(hist_landslide, origin="lower", extent=[x_edges[1], x_edges[-1], y_edges[1], y_edges[-1]], aspect="auto", cmap="Reds")
     plt.colorbar(label=r"#Landslides")
     plt.xlabel(feats[0], fontsize=12)
     plt.ylabel(feats[1], fontsize=12)
@@ -137,13 +137,10 @@ if __name__ == "__main__":
     plt.close()
 
     fig = plt.figure()
-    lvls = [p for p in np.arange(0.1, 1.01, 0.1) if p != prob_threshold]
+    lvls = [p for p in np.arange(0.1, 1.01, 0.1)]
     contours = plt.contour(x_grid, y_grid, landslide_probs, levels=lvls, colors='k')
     plt.clabel(contours, inline=True, fontsize=8, fmt="%.1f")
-    contours_threshold = plt.contour(x_grid, y_grid, landslide_probs, levels=[0.5],
-                                     colors='b', linewidths=2)
-    plt.clabel(contours_threshold, inline=True, fontsize=8, fmt="%.1f")
-    plt.imshow(landslide_ratio.T, origin="lower", extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]], aspect="auto", cmap="Reds")
+    plt.imshow(landslide_ratio, origin="lower", extent=[x_edges[1], x_edges[-1], y_edges[1], y_edges[-1]], aspect="auto", cmap="Reds")
     plt.colorbar(label=r"$\frac{Landslides}{Landslides+Survivals}$")
     plt.xlabel(feats[0], fontsize=12)
     plt.ylabel(feats[1], fontsize=12)
@@ -156,7 +153,7 @@ if __name__ == "__main__":
     fig = plt.figure()
     threshold_line = opt_threshold_params - x_grid
     plt.plot(x_grid, threshold_line, c="g", label="Threshold")
-    plt.imshow(landslide_ratio.T, origin="lower", extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]], aspect="auto", cmap="Reds")
+    plt.imshow(landslide_ratio.T, origin="lower", extent=[x_edges[1], x_edges[-1], y_edges[1], y_edges[-1]], aspect="auto", cmap="Reds")
     plt.colorbar(label=r"$\frac{Landslide}{Landslide+Survival}$")
     plt.xlabel(feats[0], fontsize=12)
     plt.ylabel(feats[1], fontsize=12)